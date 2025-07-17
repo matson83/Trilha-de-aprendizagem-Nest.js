f24b7eb0715d7562a34ccca0b4a32eeb3d1b1d97
matson83 <matsonjunior194@gmail.com>
2025-07-17 17:38:38 -0300

    Estudos sobre DTO para validação

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,15 +4,18 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
 } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Controller("users")
 export class UserController {
   @Post()
-  async createUser(@Body() body) {
-    return { body };
+  async createUser(@Body() { name, email, password }: CreateUserDto) {
+    return { name, email, password };
   }
 
   @Get()
@@ -21,17 +24,20 @@ export class UserController {
   }
 
   @Get(":id")
-  async getUserById(@Param() param) {
-    return { user: { param } };
+  async getUserById(@Param("id", ParseIntPipe) id) {
+    return { user: {}, id };
   }
 
   @Patch(":id")
-  async updateUser(@Param() param, @Body() body) {
-    return { user: { param, body } };
+  async updateUser(
+    @Param("id", ParseIntPipe) id,
+    @Body() { name, email, password }: UpdateUserDto,
+  ) {
+    return { user: { id, name, email, password } };
   }
 
   @Delete(":id")
-  async deleteUser(@Param() param) {
-    return { deleted: { param } };
+  async deleteUser(@Param("id", ParseIntPipe) id) {
+    return { id };
   }
 }
